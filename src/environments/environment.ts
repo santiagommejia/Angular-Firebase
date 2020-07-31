002c6009968f408a4ecf4ec9ba60049683540343
Santiago Mejia <santiagommejia@gmail.com>
2020-07-31 19:26:20 -0400

    minor changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "your-firebase-credentials-here",
-    authDomain: "your-firebase-credentials-here",
-    databaseURL: "your-firebase-credentials-here",
-    projectId: "your-firebase-credentials-here",
-    storageBucket: "your-firebase-credentials-here",
-    messagingSenderId: "your-firebase-credentials-here",
-    appId: "your-firebase-credentials-here",
-    measurementId: "your-firebase-credentials-here"
+    apiKey: "AIzaSyCP2F5yMUUt_wTRVa6X4VdZKZRhK2-hHnI",
+    authDomain: "angular-firebase-ea249.firebaseapp.com",
+    databaseURL: "https://angular-firebase-ea249.firebaseio.com",
+    projectId: "angular-firebase-ea249",
+    storageBucket: "angular-firebase-ea249.appspot.com",
+    messagingSenderId: "165211135503",
+    appId: "1:165211135503:web:136e118a06aa93665d8d7a",
+    measurementId: "G-0C7NJW2R0T"
   }
 };
 
