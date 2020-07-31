723d1224483448febe6b35379d4777667822a784
Santiago Mejia <santiagommejia@gmail.com>
2020-07-31 19:25:00 -0400

    minor changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "your-firebase-credentials-here",
+    authDomain: "your-firebase-credentials-here",
+    databaseURL: "your-firebase-credentials-here",
+    projectId: "your-firebase-credentials-here",
+    storageBucket: "your-firebase-credentials-here",
+    messagingSenderId: "your-firebase-credentials-here",
+    appId: "your-firebase-credentials-here",
+    measurementId: "your-firebase-credentials-here"
+  }
 };
